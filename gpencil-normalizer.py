86e6bb23e89e00ef36e41550a36eec303025cf3e
nepia <kapibara19@gmail.com>
2020-12-04 23:47:27 +0900

    任意ポイント数にストロークをサンプリングするオペレータを作成
    基礎機能の一つができたぞい

--- gpencil-normalizer.py
+++ gpencil-normalizer.py
@@ -1,7 +1,5 @@
-# from mathutils import Vector
-# from bpy_extras.object_utils import AddObjectHelper, object_data_add
-# from bpy.props import FloatVectorProperty
-# from bpy.types import Operator
+from bpy.props import FloatProperty, IntProperty, EnumProperty
+from bpy.types import Operator
 
 import bpy
 import numpy as np
@@ -9,7 +7,7 @@ import numpy as np
 
 # 2つのベクトルから長さ(ノルム?)を求める。わからなくなるのでラップしてる
 def calc_vector3_length(a: "Vector3", b: "Vector3") -> float:
-    vec = np.array(b - a)
+    vec = np.array(b - a, dtype="float64")
     return np.linalg.norm(vec)
 
 
@@ -17,12 +15,14 @@ def calc_vector3_length(a: "Vector3", b: "Vector3") -> float:
 def calc_stroke_length_and_point(gp_stroke: bpy.types.GPencilStroke) -> (float, int):
     vectors = [p.co for p in gp_stroke.points]
     point_count: int = len(vectors)
-    norms = np.zeros(point_count)
+    norms = np.zeros(point_count, dtype="float64")
     for index in range(point_count-1):
         vec_length = calc_vector3_length(vectors[index], vectors[index+1])
         norms[index] = vec_length
 
-    return np.sum(norms), point_count
+    length = np.sum(norms, dtype="float64")
+
+    return length, point_count
 
 
 # 複数フレーム内の１ストロークの長さとポイント最大数を返す
@@ -33,7 +33,7 @@ def calc_frames_stroke_length_and_count(gp_frames: bpy.types.GPencilFrames, stro
     counts: [int] = [0] * len(gp_frames)
 
     for i, frame in enumerate(gp_frames):
-        print("frame ",i,"stroke",strokes_index)
+        print("frame ", i, "stroke", strokes_index)
         stroke_length, count = calc_stroke_length_and_point(
             frame.strokes[strokes_index])
         lengths[i] = stroke_length
@@ -56,79 +56,49 @@ def get_max_count_by_selected_frames(counts: [int], select_frame_indexs: [bool])
             max_count = count
     return max_count
 
-
-"""(flag_select_only and select_frame_indexs[frame_index]
-フラグが有効なら選択範囲内でのポイント最大数に合わせてリサンプルする
-
-"""
+# ストロークをポイント数でサンプリングする
 
 
-def normalize_gpencil(gp_frames: bpy.types.GPencilFrames, flag_select_only: bool):
-    # フレームの選択状態を一括取得
-    select_frame_indexs: [bool] = get_selected_frame_index(gp_frames)
-    # フレームごとストローク数
-    strokes_counts = [len(frame.strokes) for frame in gp_frames]
-    # フレーム総数
-    frames_length = len(gp_frames)
+def stroke_count_resampler(gp_stroke: bpy.types.GPencilStroke, result_count: int) -> (int, float, int, int):
+    # 長さとポイント数を計算
+    src_length, src_count = calc_stroke_length_and_point(gp_stroke)
+    # サンプリングレートを決定
+    sample_length = src_length / (result_count-1)
+    # 適当なオフセットをつける 意味なかったかも
+    offset = sample_length / (result_count*2)
+    sample_length = sample_length + offset
+    # サンプリング実行
+    gp_stroke.select = True
+    bpy.ops.gpencil.stroke_sample(length=sample_length)
 
-    def resample_stroke(frame_index, stroke_index):
-        if frames_length == 1:
-            print("スキップ",frames_length,stroke_index)
-            return 0
-
-        
-        print("resample_stroke")
-        lengths, counts, max_count = calc_frames_stroke_length_and_count(
-            gp_frames, stroke_index)
-
-        # 選択フレームのみリサンプルしたいときのmax_countを取得
-        if flag_select_only:
-            max_count = get_max_count_by_selected_frames(
-                counts, select_frame_indexs)
-
-        resample_length: float = lengths[frame_index] / max_count
-        # bpy.ops.gpencil.stroke_sample()でストロークをリサンプルできる
-        # select = Trueになってるストローク全部にかかるので選択解除してからやる
-        for stroke in gp_frames[frame_index].strokes:
-            stroke.select = False
-
-        gp_frames[frame_index].strokes[stroke_index].select = True
-        bpy.ops.gpencil.stroke_sample(length=resample_length)
-
-    # 選択条件を判定して、すべての有効ストロークをリサンプルする
-    print("frames_length",frames_length)
-    for frame_index in range(frames_length):
-        # フラグが有効かつ選択フレームかのフラグ
-        is_select_enable: bool = flag_select_only and select_frame_indexs[frame_index]
-        if (flag_select_only == False) or is_select_enable:
-            for _stroke_index in range(strokes_counts[frame_index]):
-                resample_stroke(frame_index, _stroke_index)
+    dst_count = len(gp_stroke.points)
+    return src_count, src_length, dst_count, result_count
 
 
-# operatorから呼ぶやつ
-def ops_execute(select_only:bool, context:bpy.types.Context):
-    # active_obj = context.active_object
-    if context.active_object.type == "GPENCIL":
-        gp_object = context.active_object.data
-        gp_layers = gp_object.layers
-        active_layers_index = [layer.select for layer in gp_layers]
-        layer_names = []
+def get_layers_select_state(gp_data: bpy.types.GreasePencil) -> [bool]:
+    return [layer.select for layer in gp_data.layers]
 
-        if select_only:
-            for i, layer in enumerate(gp_layers):
-                if active_layers_index[i]:
-                    layer_names.append(layer.info)
-                    print(layer.info)
-                    normalize_gpencil(gp_frames=layer.frames,
-                                      flag_select_only=True)
-        else:
-            for i, layer in enumerate(gp_layers):
-                layer_names.append(layer.info)
-                print(layer.info)
-                normalize_gpencil(gp_frames=layer.frames,
-                                  flag_select_only=False)
 
-    return layer_names
+def get_frames_select_state(gp_frames: bpy.types.GPencilFrames) -> [bool]:
+    return [frame.select for frame in gp_frames]
+
+
+def get_strokes_select_state(gp_strokes: bpy.types.GPencilStrokes):
+    return [stroke.select for stroke in gp_strokes]
+
+
+def get_strokes_select_state_by_frames(gp_frames: bpy.types.GPencilFrames) -> ([[bool]]):
+    return [get_strokes_select_state(frame.strokes) for frame in gp_frames]
+
+
+def get_select_state(gp_data: bpy.types.GreasePencil):
+    layers = gp_data.layers
+    layer_state = get_layers_select_state(gp_data)
+    # [layer0[bool,bool],layre1[,,,]]
+    frame_state = [get_frames_select_state(layer.frames) for layer in layers]
+    stroke_state = [get_strokes_select_state_by_frames(
+        layer.frames) for layer in layers]  # [l[f[s:bool,,],,],,]
+    return layer_state, frame_state, stroke_state
 
 
 # アドオン用のいろいろ
@@ -147,46 +117,54 @@ bl_info = {
 }
 
 
-class NP_GPN_OT_GPencilStrokeNormalizer(bpy.types.Operator):
+class NP_GPN_OT_GPencilStrokeCountResampler(bpy.types.Operator):
 
-    bl_idname = "gpencil.np_gpencil_stroke_normalizer"
-    bl_label = "stroke normalizer"
-    bl_description = "すべてのストロークを正規化する"
+    bl_idname = "gpencil.np_gpencil_stroke_count_resampler"
+    bl_label = "stroke count resampler"
+    bl_description = "ストロークをポイント数でサンプリングする"
     bl_options = {'REGISTER', 'UNDO'}
+
+    amount: IntProperty(
+        name="ポイント数",
+        description="ポイント数を設定します",
+        default=100,
+    )
 
     # メニューを実行したときに呼ばれるメソッド
     def execute(self, context):
-        layer_names = ops_execute(select_only=False,context=context)
 
-        self.report(
-            {'INFO'}, "gpencil normalizer: resampled {}".format(layer_names))
-        return {'FINISHED'}
+        def _command(gp_stroke):
+            src_count, src_length, dst_count, _result_count = stroke_count_resampler(
+                gp_stroke, result_count)
 
-class NP_GPN_OT_GPencilStrokeNormalizerSelectOnly(bpy.types.Operator):
+            self.report(
+                {'INFO'}, "resampled:src_count:{}, src_length:{}, dst_count:{},res_count:{}".format(
+                    src_count, src_length, dst_count, _result_count)
+            )
+        # bpy.context.active_object.data = bpy.data.grease_pencils['Stroke']
+        gp_data = context.active_object.data
+        result_count = self.amount
 
-    bl_idname = "gpencil.np_gpencil_stroke_normalizer_select_only"
-    bl_label = "stroke normalizer(select only)"
-    bl_description = "選択されたレイヤー、フレームのストロークを正規化する"
-    bl_options = {'REGISTER', 'UNDO'}
+        layer_state, frame_state, stroke_state = get_select_state(gp_data)
 
-    # メニューを実行したときに呼ばれるメソッド
-    def execute(self, context):
-        layer_names = ops_execute(select_only=True,context=context)
+        for li, layer in enumerate(gp_data.layers):
+            if layer_state[li]:
+                for fi, frame in enumerate(layer.frames):
+                    if frame_state[li][fi]:
+                        for si, stroke in enumerate(frame.strokes):
+                            if stroke_state[li][fi][si]:
+                                _command(stroke)
 
-        self.report(
-            {'INFO'}, "gpencil normalizer: resampled {}".format(layer_names))
         return {'FINISHED'}
 
 
 def menu_fn(self, context):
     self.layout.separator()
-    self.layout.operator(NP_GPN_OT_GPencilStrokeNormalizer.bl_idname)
-    self.layout.operator(NP_GPN_OT_GPencilStrokeNormalizerSelectOnly.bl_idname)
+    self.layout.operator(NP_GPN_OT_GPencilStrokeCountResampler.bl_idname)
 
 
 classes = [
-    NP_GPN_OT_GPencilStrokeNormalizer,
-    NP_GPN_OT_GPencilStrokeNormalizerSelectOnly,
+    NP_GPN_OT_GPencilStrokeCountResampler,
 ]
 
 
