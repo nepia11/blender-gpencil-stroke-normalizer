9bd69a3f961dbcbb0066cb0839606db4d6157dd4
nepia11 <kapibara19@gmail.com>
2021-03-19 23:28:32 +0900

    add average color

--- lib/ops_capture_color.py
+++ lib/ops_capture_color.py
@@ -1,5 +1,7 @@
 import bpy
 import bgl
+import math
+import numpy as np
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -7,17 +9,37 @@ logger = getLogger(__name__)
 translation = bpy.app.translations.pgettext
 
 
-def capture_under_cursor(buffer, mouse_x=0, mouse_y=0, type_flg="i") -> list:
+def calc_kernel(x, y, size):
+    if size == 1:
+        return x, y, size
+    half = size / 2
+    p1_x = math.floor(x - half)
+    p1_y = math.floor(y - half)
+    w_h = size
+    return p1_x, p1_y, w_h
+
+
+def average_color(color: list):
+    """ 複数サンプルした時に平均色を求めたい """
+    c = color
+    avg_color = [c[i : i + 4] for i in range(0, len(c), 4)]
+    avg_color = np.array(avg_color)
+    avg = np.average(avg_color, axis=0)
+    return avg
+
+
+def capture_under_cursor(buffer, mouse_x=0, mouse_y=0, size=1, type_flg="i") -> list:
     """
     フラットなrgba(float)のlistを返す
     """
+    k = calc_kernel(mouse_x, mouse_y, size)
     # GL_FLOATでバッファ作って読むと馬鹿みたいに重いのでGL_BYTE,GL_UNSIGNED_BYTEになってる
     bgl.glReadBuffer(bgl.GL_FRONT)
     bgl.glReadPixels(
-        mouse_x,
-        mouse_y,
-        1,
-        1,
+        k[0],
+        k[1],
+        k[2],
+        k[2],
         bgl.GL_RGBA,
         bgl.GL_UNSIGNED_BYTE,
         buffer,
@@ -45,18 +67,27 @@ class TEMPLATE_OT_CaptureColor(bpy.types.Operator):
     bl_idname = "template.capture_color"
     bl_label = translation("my operator")
     bl_description = "operator description"
-    bl_options = {"REGISTER", "UNDO"}
-
-    buffer = create_buffer()
+    # bl_options = {"REGISTER", "UNDO"}
+    kernel_size = 3
+    buffer = create_buffer(kernel_size, kernel_size)
     keymaps = []
     # イベントを受け取りたいときはexecuteの代わりにinvokeが使える
 
     def invoke(self, context, event):
-        color = capture_under_cursor(self.buffer, event.mouse_x, event.mouse_y, "f")
-        context.tool_settings.gpencil_paint.brush.color = color[:3]
+        gpencil_paint = context.tool_settings.gpencil_paint
+        _show_brush = gpencil_paint.show_brush
+        gpencil_paint.show_brush = False
+        color = capture_under_cursor(
+            self.buffer, event.mouse_x, event.mouse_y, self.kernel_size, "f"
+        )
+        color = average_color(color)
+        gpencil_paint.brush.color = color[:3]
+        gpencil_paint.show_brush = _show_brush
+
         # brushes = [b for b in bpy.data.brushes]
         # for b in brushes:
         #     b.color = (color[:3])
+
         # logging
         logger.debug(color)
         # infoにメッセージを通知
@@ -68,6 +99,7 @@ class TEMPLATE_OT_CaptureColor(bpy.types.Operator):
     def register(cls):
         wm = bpy.context.window_manager
         kc = wm.keyconfigs.addon
+        kc.keys()
         if kc:
             # [3Dビューポート] スペースのショートカットキーとして登録
             km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
